51890f535be0fb1e2c1c1c220ebb550ab305bc0b
Maria Fernanda Castro <mafercc241@gmail.com>
2024-12-10 14:46:11 -0800

    Alternar aside configuración IMG y TXT

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("Hola")
+
+// SECCIÓN PARA ABRIR BARRAS LATERALES//
+
+// Guardar botones en constante
+const botonImg = document.getElementById('botonConfiguracionImagen');
+const botonTexto = document.getElementById('botonConfiguracionTexto');
+const contenedorAsideImg = document.getElementById('contenedorAsideImg');
+const contenedorAsideTexto = document.getElementById('contenedorAsideTexto');
+
+// Función para abrir y cerrar conteendores de configuración
+function alternarAside(mostrar, ocultar) {
+  mostrar.style.display = 'block';
+  ocultar.style.display = 'none';
+}
+
+// Eventos //
+// Mostrar configuración de imagen y ocultar la de texto
+botonImg.addEventListener('click', () => {
+  alternarAside(contenedorAsideImg, contenedorAsideTexto);
+});
+
+// Mostrar configuración de texto y ocultar la de imagen
+botonTexto.addEventListener('click', () => {
+  alternarAside(contenedorAsideTexto, contenedorAsideImg);
+});
+
+// Función para configuración inicial mostrar configuración imagen 
+alternarAside(contenedorAsideImg, contenedorAsideTexto); 
